c5ca41cf742d3763257b7c071892a9c6159e572e
Andres Silva <silvax@gmail.com>
2017-06-06 13:48:44 -0400

    adding the files for the first time

---
+++ recipes/import-parameter-store.rb
@@ -0,0 +1,30 @@
+Chef::Log.info("******Retrieving parameters from store.******")
+
+ruby_block "load-parameters" do
+  block do
+    require 'aws-sdk'
+    #stack = search("aws_opsworks_stack").first
+    ssm = Aws::SSM::Client.new
+
+    allparameters = ssm.describe_parameters({
+      # filters: [
+      #   {
+      #     key: "Name", # accepts Name, Type, KeyId
+      #     values: ["^ow_#{stack}"], # required
+      #   },
+      # ],
+      # next_token: "NextToken",
+    })
+
+    for each_parameter in all_parameters
+
+      # resp = ssm.get_parameters({
+      #   names: ["ow_#{stack}"], # required
+      #   with_decryption: false,
+      # })
+      puts "#{each_parameter}"
+
+    end
+  end
+  action :run
+end
